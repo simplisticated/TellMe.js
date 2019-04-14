55e437ccfe821929871b5d75b7350c8ad7d7410c
Igor Matyushkin <igormatyushkin014@gmail.com>
2019-04-14 18:44:24 +0300

    Updated demo project

--- demo/main.js
+++ demo/main.js
@@ -1,9 +1,10 @@
 var speakButton = document.getElementById("speakButton");
 speakButton.onclick = function() {
-	tellMeIn("en-US").say(
-		"Hello! How are you doing?",
-		function() {
+	tellMeIn("en-US")
+		.say(
+			"Hello! How are you doing?"
+		)
+		.onFinished(function() {
 			console.log("Finished");
-		}
-	);
+		});
 };
